b68e9ff6139b26ba40c1fc669934ab2c7eae6409
ipad6202 <78675126+ipad6202@users.noreply.github.com>
2026-05-20 20:40:25 -0400

    Added Flywheel

--- src/main/java/frc/robot/Mechanisms/Shooter/Flywheel/Flywheel.java
+++ src/main/java/frc/robot/Mechanisms/Shooter/Flywheel/Flywheel.java
@@ -1,7 +1,65 @@
 package frc.robot.Mechanisms.Shooter.Flywheel;
 
+import static org.wpilib.units.Units.RadiansPerSecond;
+
+import java.util.function.Supplier;
+
+import org.wpilib.command3.Command;
 import org.wpilib.command3.Mechanism;
+import org.wpilib.command3.NeedsNameBuilderStage;
+import org.wpilib.command3.Scheduler;
+import org.wpilib.units.measure.AngularVelocity;
+import org.wpilib.units.measure.Voltage;
+
+import frc.robot.Mechanisms.Shooter.Flywheel.FlywheelIO.FlywheelIOInputs;
 
 public class Flywheel extends Mechanism {
+
+    private final FlywheelIO io;
+    private FlywheelIOInputs inputs = new FlywheelIOInputs();
+
+    private AngularVelocity goalVelocity = RadiansPerSecond.zero();
+
+    public Flywheel(FlywheelIO io) {
+        this.io = io;
+
+        Scheduler.getDefault().addPeriodic(() -> {
+            io.updateInputs(inputs);
+        });
+    }
+
+    private NeedsNameBuilderStage runVoltageCommand(Supplier<Voltage> volts) {
+        return runRepeatedly(() -> io.setVoltage(volts.get()))
+                .whenCanceled(io::stop);
+    }
+
+    private NeedsNameBuilderStage runVelocityCommand(Supplier<AngularVelocity> velocity) {
+        return runRepeatedly(() -> {
+            goalVelocity = velocity.get();
+            io.setVelocity(velocity.get());
+        }).whenCanceled(() -> {
+            goalVelocity = RadiansPerSecond.zero();
+            io.stop();
+        });
+    }
+
+    public Command runClosePresetVelocity() {
+        return runVelocityCommand(() -> RadiansPerSecond.of(200))
+                .named("Flywheel/ClosePresetVelocity");
+    }
+
+    public Command runFarPresetVelocity() {
+        return runVelocityCommand(() -> RadiansPerSecond.of(300))
+                .named("Flywheel/FarPresetVelocity");
+    }
+
+    public Command runIdleVelocity() {
+        return runVelocityCommand(() -> RadiansPerSecond.of(50))
+                .named("Flywheel/IdleVelocity");
+    }
+
+
+
+
     
 }
---
+++ src/main/java/frc/robot/Mechanisms/Shooter/Flywheel/FlywheelIO.java
@@ -0,0 +1,41 @@
+package frc.robot.Mechanisms.Shooter.Flywheel;
+
+import static org.wpilib.units.Units.Amps;
+import static org.wpilib.units.Units.Celsius;
+import static org.wpilib.units.Units.Radians;
+import static org.wpilib.units.Units.RadiansPerSecond;
+import static org.wpilib.units.Units.Volts;
+
+import org.wpilib.units.measure.Angle;
+import org.wpilib.units.measure.AngularVelocity;
+import org.wpilib.units.measure.Current;
+import org.wpilib.units.measure.Temperature;
+import org.wpilib.units.measure.Voltage;
+
+public interface FlywheelIO {
+    
+    public static class FlywheelIOInputs {
+        public boolean leaderConnected = false;
+        public Angle leaderAngleRads = Radians.zero();
+        public AngularVelocity leaderVelocityRadsPerSec = RadiansPerSecond.zero();
+        public Voltage leaderAppliedVolts = Volts.zero();
+        public Current leaderCurrentAmps = Amps.zero();
+        public Temperature leaderTemperatureCelsius = Celsius.zero();
+
+        public boolean followerConnected = false;
+        public Angle followerAngleRads = Radians.zero();
+        public AngularVelocity followerVelocityRadsPerSec = RadiansPerSecond.zero();
+        public Voltage followerAppliedVolts = Volts.zero();
+        public Current followerCurrentAmps = Amps.zero();
+        public Temperature followerTemperatureCelsius = Celsius.zero();
+    }
+
+    public default void updateInputs(FlywheelIOInputs inputs) {}
+
+    public default void setVoltage(Voltage volts) {}
+
+    public default void setVelocity(AngularVelocity velocity) {}
+
+    public default void stop() {}
+
+}
